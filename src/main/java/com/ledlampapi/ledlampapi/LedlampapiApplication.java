6378c82caf2a763d38f7db09a1043d8a48627426
Dorian Arnoux <darnouxp.live@gmail.com>
2020-11-23 08:31:49 +0100

    First commit

---
+++ src/main/java/com/ledlampapi/ledlampapi/LedlampapiApplication.java
@@ -0,0 +1,13 @@
+package com.ledlampapi.ledlampapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedlampapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedlampapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ledlampapi/ledlampapi/LedlampapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledlampapi.ledlampapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedlampapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
