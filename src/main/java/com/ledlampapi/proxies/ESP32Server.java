8034e25e7fa642c407dc31141aca03e3a75dd974
Dorian Arnoux <darnouxp.live@gmail.com>
2021-01-09 14:44:29 +0100

    ESP32Server

--- src/main/java/com/ledlampapi/proxies/ESP32Server.java
+++ src/main/java/com/ledlampapi/proxies/ESP32Server.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="esp32", url = "TODO URL du server ")
 public interface ESP32Server {
-    @RequestMapping(method = RequestMethod.POST, value = "/search.php?key="+pk+"&format=json")
-    List<LocationIQAPIResponse> getLatLongTrash(@RequestParam("q") String address);
+    @RequestMapping(method = RequestMethod.POST, value = "/changeColor")
+    void setColor(@RequestParam("q") String jsonColor);
 }
